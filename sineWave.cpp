f7031f9c157a23a998cdb06b4b6e6de6373247c4
Erik Williams <epwilliams@csu.fullerton.edu>
2023-07-10 17:08:20 -0700

    frequency and duration from terminal

--- sineWave.cpp
+++ sineWave.cpp
@@ -1,25 +1,33 @@
 #include <sndfile.h>
 
 #include <cmath>
+#include <cstdlib>  // for atof
 #include <fstream>
 #include <iostream>
 #include <vector>
 
 const int SAMPLE_RATE = 44100;
 const double TWO_PI = M_PI * 2;
-const double FREQUENCY = 440.0;
 const double MAX_AMPLITUDE = 0.9;
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " <frequency> <duration>\n";
+    return 1;
+  }
+
+  double frequency = std::atof(argv[1]);
+  int duration = std::atoi(argv[2]);
+
   // Prepare to generate sine wave
-  const double increment = FREQUENCY * TWO_PI / SAMPLE_RATE;
+  const double increment = frequency * TWO_PI / SAMPLE_RATE;
   double x = 0;
 
   // Create a buffer to hold the samples
-  std::vector<short> buffer(SAMPLE_RATE);
+  std::vector<short> buffer(SAMPLE_RATE * duration);
 
-  // Generate one second of audio
-  for (int i = 0; i < SAMPLE_RATE; i++) {
+  // Generate audio
+  for (int i = 0; i < SAMPLE_RATE * duration; i++) {
     double value = sin(x);
     x += increment;
 
@@ -34,15 +42,15 @@ int main() {
   sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
 
   // Open the sound file for writing
-  SNDFILE *outfile = sf_open("sine.wav", SFM_WRITE, &sfinfo);
+  SNDFILE* outfile = sf_open("sine.wav", SFM_WRITE, &sfinfo);
 
   // Write the generated samples to the file
-  sf_write_short(outfile, &buffer[0], SAMPLE_RATE);
+  sf_write_short(outfile, &buffer[0], SAMPLE_RATE * duration);
 
   // Close the file
   sf_close(outfile);
 
-  std::cout << "Wrote one second of a " << FREQUENCY
+  std::cout << "Wrote " << duration << " second(s) of a " << frequency
             << "Hz sine wave to 'sine.wav'" << std::endl;
 
   return 0;
