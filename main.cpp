55be77f8d47054535fadc4a08b5e5a6bf5c0a1b8
Erik Williams <epwilliams@csu.fullerton.edu>
2023-07-20 13:51:53 -0700

    Added error handling

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 #include "waveForms.h"
 
 const int SAMPLE_RATE = 44100;
@@ -21,22 +20,17 @@ int main(int argc, char *argv[])
   std::string waveform = argv[1];
   int duration = std::atoi(argv[2]);
 
-  // Create a buffer to hold the samples
   std::vector<short> buffer(SAMPLE_RATE * duration);
 
-  // For each frequency argument...
   for (int arg = 3; arg < argc; ++arg)
   {
-
     double frequency = std::atof(argv[arg]);
 
-    // Generate audio
     for (int i = 0; i < SAMPLE_RATE * duration; i++)
     {
       double value;
-      double x = (double)i / SAMPLE_RATE * frequency; // phase
+      double x = (double)i / SAMPLE_RATE * frequency;
 
-      // Select waveform
       if (waveform == "sine")
         value = sine_wave(x);
       else if (waveform == "square")
@@ -51,24 +45,31 @@ int main(int argc, char *argv[])
         return 1;
       }
 
-      // Add to the existing value in the buffer, scaling to avoid clipping
       buffer[i] += value * MAX_AMPLITUDE * 32767.0 / (argc - 3);
     }
   }
 
-  // Write to .wav file
   SF_INFO sfinfo;
   sfinfo.samplerate = SAMPLE_RATE;
   sfinfo.channels = 1;
   sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
 
-  // Open the sound file for writing
   SNDFILE *outfile = sf_open("audio.wav", SFM_WRITE, &sfinfo);
 
-  // Write the generated samples to the file
-  sf_write_short(outfile, &buffer[0], SAMPLE_RATE * duration);
+  if (!outfile)
+  {
+    std::cerr << "Error opening file: " << sf_strerror(NULL) << std::endl;
+    return -1;
+  }
 
-  // Close the file
+  sf_count_t count = sf_write_short(outfile, &buffer[0], SAMPLE_RATE * duration);
+  if (count != SAMPLE_RATE * duration)
+  {
+    std::cerr << "Error writing to file: " << sf_strerror(outfile) << std::endl;
+    sf_close(outfile);
+    return -1;
+  }
+
   sf_close(outfile);
 
   std::cout << "Wrote " << duration << " second(s) of a " << argv[3]
